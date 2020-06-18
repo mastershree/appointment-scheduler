20adf98cb47775c3706047ef278b4ecd1b796f26
Shrikant Gawas <gawasshree@gmail.com>
2020-06-19 01:43:15 +0530

    Minor issues fixed

--- index.js
+++ index.js
@@ -56,14 +56,14 @@ const JWT_SECRET = "password";
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname + "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.get("/app*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 /*
