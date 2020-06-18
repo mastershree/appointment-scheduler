54fa7f00814c2412cbd655306e77c003e7739877
Shrikant Gawas <gawasshree@gmail.com>
2020-06-19 02:01:49 +0530

    Minor issues fixed

--- index.js
+++ index.js
@@ -58,10 +58,10 @@ app.use(bodyParser.json());
 
 console.log(__dirname);
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 /*
 app.get("/app*", (req, res) => {
