df801c001306899a8c1376887dbcb31473b82c58
Shrikant Gawas <gawasshree@gmail.com>
2020-07-04 01:13:20 +0530

    minor change against password reset url

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ console.log(__dirname);
 
 app.use(express.static("client/build"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
