1a90ed246601c67cfb0e9db191b81d37404ff3e5
Shrikant Gawas <gawasshree@gmail.com>
2020-07-03 15:36:37 +0530

    minor change in url of reset password

--- index.js
+++ index.js
@@ -489,7 +489,7 @@ app.post("*/api/schedule_event/:eventId", (req, res) => {
 });
 
 //Server listening
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
 });
